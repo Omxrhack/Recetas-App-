03f86cd57e5ab79c9f18b9c478ad65b27ae6852d
Omar Bermejo Osuna <l21170203@culiacan.tecnm.mx>
2025-09-12 13:59:12 -0700

    LunchScreen

--- CRUD/EJ1PT3DISPOSITIVOS/Pages/LoginPage.swift
+++ CRUD/EJ1PT3DISPOSITIVOS/Pages/LoginPage.swift
@@ -15,77 +15,60 @@ struct LoginPage: View {
     @EnvironmentObject private var app: AppNavigation
     
     var body: some View {
-        ZStack{
-           
-            ZStack{
-                Circle()
-                    .fill(Color("CustomOrange"))
+        
+        NavigationStack{
+            
+            VStack(spacing: 16) {
                 
-            }
-            .offset(x: 100,y:-200)
-
-            ZStack{
-                Image("Iconapp")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 400, height: 400)
-                   
-            }
-            .offset(x: 1,y:0)
-        }
-        ZStack{
-            NavigationStack{
+                Text("Iniciar Sesión")
+                    .font(.title)
+                    .bold()
                 
-                VStack(spacing: 16) {
+                TextField("Usuario", text: $username)
+                    .textInputAutocapitalization(.never)
+                    .disableAutocorrection(true)
+                    .padding()
+                    .background(Color.gray.opacity(0.15))
+                    .cornerRadius(8)
+                
+                SecureField("Contraseña", text: $password)
+                    .padding()
+                    .background(Color.gray.opacity(0.15))
+                    .cornerRadius(8)
+                
+                Button("Entrar") {
                     
-                    Text("Iniciar Sesión")
-                        .font(.title)
-                        .bold()
-                    
-                    TextField("Usuario", text: $username)
-                        .textInputAutocapitalization(.never)
-                        .disableAutocorrection(true)
-                        .padding()
-                        .background(Color.gray.opacity(0.15))
-                        .cornerRadius(8)
-                    
-                    SecureField("Contraseña", text: $password)
-                        .padding()
-                        .background(Color.gray.opacity(0.15))
-                        .cornerRadius(8)
-                    
-                    Button("Entrar") {
+                    Task {
+                        let success = DatabaseManager.shared.loginUser(username: username, password: password)
                         
-                        Task {
-                            let success = DatabaseManager.shared.loginUser(username: username, password: password)
-                            
-                            await MainActor.run {
-                                if success {
-                                    app.login(username: username)
-                                } else {
-                                    message = "Usuario o contraseña incorrectos"
-                                }
+                        await MainActor.run {
+                            if success {
+                                app.login(username: username)
+                            } else {
+                                message = "Usuario o contraseña incorrectos"
                             }
                         }
                     }
-                    .padding()
-                    .buttonStyle(.borderedProminent)
-                    
-                    if !message.isEmpty {
-                        Text(message)
-                            .foregroundColor(.red)
-                    }
-                    Button("¿No tienes cuenta? Regístrate") {
-                        showRegister = true
-                    }
-                }
-                .navigationDestination(isPresented: $showRegister) {
-                    RegisterPage()
                 }
                 .padding()
+                .buttonStyle(.borderedProminent)
+                
+                if !message.isEmpty {
+                    Text(message)
+                        .foregroundColor(.red)
+                }
+                Button("¿No tienes cuenta? Regístrate") {
+                    showRegister = true
+                }
             }
+            .navigationDestination(isPresented: $showRegister) {
+                RegisterPage()
+            }
+            .padding()
         }
-    }}
+        
+    }
+}
 #Preview {
     LoginPage()
         .environmentObject(AppNavigation())
